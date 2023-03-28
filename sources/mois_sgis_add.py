df595fc611a3abf07bdf018459a48af89fe2aea3
Noh Hyeogjin <129146876+hjnoh11@users.noreply.github.com>
2023-03-28 17:39:39 +0900

    경로 수정

--- sources/mois_sgis_add.py
+++ sources/mois_sgis_add.py
@@ -3,14 +3,14 @@ import csv
 year=2022
 
 mapping_table={}
-f1=open("./"+str(year)+"_sgis_attributes_table.csv",'r',encoding='utf-8-sig')
+f1=open("../"+str(year)+"_sgis_attributes_table.csv",'r',encoding='utf-8-sig')
 reader=csv.reader(f1)
 for line in reader:
 	mapping_table[line[2]]=line[0]
 f1.close()
 
-f2=open("./"+str(year)+"_06_mois_cjs_merge.csv",'r',encoding='utf-8-sig')
-f3=open("./"+str(year)+"_06_pop.csv",'w',encoding='utf-8',newline='')
+f2=open("../"+str(year)+"_06_mois_cjs_merge.csv",'r',encoding='utf-8-sig')
+f3=open("../"+str(year)+"_06_pop.csv",'w',encoding='utf-8',newline='')
 reader=csv.reader(f2)
 writer=csv.writer(f3)
 
